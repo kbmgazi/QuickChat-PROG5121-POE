8ec3957ee268a9ed17895b1eff181a53117f83e8
KB MGAZI <mgazikb@gmail.com>
2026-08-15 17:13:23 +0200

    Add NetBeans QuickChat project
    Add NetBeans-generated project and build files for QuickChat (QuickChat/build.xml, QuickChat/nbproject/*, manifest.mf, genfiles.properties, project.properties, build-impl.xml). Include initial Java sources quickchat.Main and quickchat.UserLogin. Update .gitignore to ignore nbproject/private directories. Sets up project structure and Ant build configuration generated by the IDE.

---
+++ QuickChat/src/quickchat/UserLogin.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quickchat;
+
+/**
+ *
+ * @author Kea
+ */
+public class UserLogin {
+    boolean x = false;
+    public boolean checkUserName() {
+        System.out.println("Enter X");
+        x = true;
+        
+        return x;
+    }
+    
+    
+}
