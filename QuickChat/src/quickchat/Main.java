b30c8f2d210cff4648ab28748761186fb1884539
KB MGAZI <mgazikb@gmail.com>
2026-08-21 15:46:31 +0200

    Add UserLogin test and implement UserLogin
    Adds a JUnit test for UserLogin and prepares the project test classpath. Changes include: 1) new QuickChat/test/quickchat/UserLoginTest.java (JUnit test scaffolding, tests add(3,3)); 2) QuickChat/src/quickchat/UserLogin.java expanded with username/password/cell validation, registration and login methods (skeletons/placeholder messages); 3) QuickChat/src/quickchat/Main.java updated to instantiate UserLogin and call add(3,3); 4) QuickChat/nbproject/project.properties updated to include JUnit on the test classpath; 5) .gitignore updated to ignore /QuickChat/build/. Some methods contain placeholder returns and may need refinement to pass tests and complete behavior.

--- QuickChat/src/quickchat/Main.java
+++ QuickChat/src/quickchat/Main.java
@@ -4,6 +4,7 @@
  */
 package quickchat;
 
+
 /**
  *
  * @author Kea
@@ -15,6 +16,11 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //Main myObj = new Main();
+        UserLogin myObj = new UserLogin();
+        
+        myObj.add(3, 3);
     }
     
 }
--- QuickChat/src/quickchat/UserLogin.java
+++ QuickChat/src/quickchat/UserLogin.java
@@ -8,13 +8,131 @@ package quickchat;
  *
  * @author Kea
  */
+import quickchat.Main;
 public class UserLogin {
-    boolean x = false;
-    public boolean checkUserName() {
-        System.out.println("Enter X");
-        x = true;
+    
+    //declarations
+    private String registeredUsername;
+    private String registeredPassword;
+    private String registeredFirstName;
+    private String registeredLastName;
+    private String registeredCellPhoneNumber;
+    
+    //username validation method 
+    public boolean checkUserName(String username) {
+        boolean hasUnderscore = false;
         
-        return x;
+        if (username.length() <= 5) {
+            for (int i = 0; i < username.length(); i++) {
+                if (username.contains("_")) {
+                    hasUnderscore = true;
+                }
+            }
+        }
+        return hasUnderscore;
+    }
+    
+    //password complexity validation method 
+    public boolean checkPasswordComplexity(String password) {
+        //declaration 
+        boolean hasCapital = false;
+        boolean hasNumber = false;
+        boolean hasSpecial = false;
+        char c;
+        
+        if (password.length() >= 8) {
+            for (int i = 0 ; i < password.length() - 1; i++) {
+                c = password.charAt(i);
+                if (Character.isUpperCase(c)) {
+                    hasCapital = true;
+                }else if (Character.isDigit(c)) {
+                    hasNumber = true;
+                } else if (!Character.isLetterOrDigit(c) && c != " ") {
+                    hasSpecial = true;
+                }
+            }
+        }
+        return hasCapital && hasNumber && hasSpecial;
+    }
+    
+    //cell phone number validation method 
+    public boolean checkCellPhone(String cellPhoneNumber) {
+        //declaration 
+          boolean isValid = false;
+          String zaCode = "";
+          
+          if (cellPhoneNumber.length() <= 10 && cellPhoneNumber.matches(zaCode)) {
+              isValid = true;
+          }
+          return isValid;
+    }
+    
+    //user registration method 
+    public String registerUser(String firstName, String lastName, String username,String password, String cellPhoneNumber ) {
+        //declaration 
+        boolean usernameValid;
+        boolean passwordValid;
+        boolean cellPhoneNumberValid;
+        String message;
+        
+        usernameValid = checkUserName(username);
+        passwordValid = checkPasswordComplexity(password);
+        cellPhoneNumberValid = checkCellPhone(cellPhoneNumber);
+        
+        //register user if username and password requirement are valid
+        if (usernameValid && passwordValid) {
+            registeredFirstName = firstName;
+            registeredLastName = lastName;
+            registeredUsername = username;
+            registeredPassword = password;
+            registeredCellPhoneNumber = cellPhoneNumber;
+            
+            message = "";
+            message = "";
+            
+            //cellphone 
+            if (cellPhoneNumberValid) {
+                message = "";
+            }
+            
+        }else{
+            if (!usernameValid) {
+                message = "";
+            }
+            if (!passwordValid) {
+                message = "";
+            }
+            if (!cellPhoneNumberValid) {
+                message = "";
+            }
+        }
+        
+        return message;
+    }
+    
+    //login authentication method 
+    public boolean loginUser(String username, String password){
+        //declaration 
+            boolean loginSuccess = false;
+            
+            if (username == registeredUsername && password == registeredPassword) {
+                loginSuccess = true;
+            }
+            
+     return loginSuccess;
+    }
+    
+    //authentication status reporter method
+    public String returnLoginStatus(boolean loginSuccess, String firstName, String lastName) {
+        //declaration 
+        String statusMessage;
+        
+        if (loginSuccess) {
+            statusMessage = "";
+        }else {
+            statusMessage = "";
+        }
+    return statusMessage;
     }
     
     
